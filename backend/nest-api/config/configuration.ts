080961b3bdd853d402b07304585994565e7c9143
Aziz <aziznal.dev@gmail.com>
2023-05-26 21:14:40 +0300

    backend: add host env variable and env types

--- backend/nest-api/config/configuration.ts
+++ backend/nest-api/config/configuration.ts
@@ -1,3 +1,17 @@
-export const configuration = () => ({
-  postgresPort: parseInt(process.env.POSTGRES_PORT, 10),
-});
+export type DbConfig = {
+  host: string;
+  port: number;
+};
+
+export type EnvConfig = {
+  db: DbConfig;
+};
+
+export const configuration = (): EnvConfig => {
+  return {
+    db: {
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+    },
+  };
+};
--- backend/nest-api/config/validation.ts
+++ backend/nest-api/config/validation.ts
@@ -3,6 +3,6 @@ import * as Joi from 'joi';
 export const validationSchema = Joi.object({
   NODE_ENV: Joi.string().valid('development', 'production'),
 
+  POSTGRES_HOST: Joi.string().default('http://localhost'),
   POSTGRES_PORT: Joi.number().default(5432),
 });
-
