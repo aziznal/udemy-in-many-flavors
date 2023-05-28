218606ed98ac6bec052b5182a208362d2dd3d223
Aziz <aziznal.dev@gmail.com>
2023-05-28 16:31:30 +0300

    backend: add class-validator for validating requests

--- backend/nest-api/src/main.ts
+++ backend/nest-api/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvConfig, NestConfig } from 'config/configuration';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
@@ -11,6 +11,13 @@ async function bootstrap() {
   const configService = app.get(ConfigService<EnvConfig>);
   const nestPort = configService.get<NestConfig>('nest').port;
   const nodeEnv = configService.get('nodeEnv');
+
+  // Enable validation globally
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   // Setting up swagger
   const swaggerConfig = new DocumentBuilder()
