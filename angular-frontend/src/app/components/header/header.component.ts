dcaf520b2ba1fe49632d8c12c7474d9cc752c455
Aziz <aziznal.dev@gmail.com>
2023-06-06 20:43:29 +0300

    angular: create header

--- angular-frontend/src/app/components/header/header.component.ts
+++ angular-frontend/src/app/components/header/header.component.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  standalone: true,
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
--- angular-frontend/src/app/layouts/home-layout/home-layout.component.ts
+++ angular-frontend/src/app/layouts/home-layout/home-layout.component.ts
@@ -7,20 +7,8 @@ import { FooterComponent } from 'src/app/components/footer/footer.component';
 @Component({
   standalone: true,
   imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent],
-  template: `
-      <header>
-        <app-header></app-header>
-      </header>
-
-      <router-outlet></router-outlet>
-
-      <footer>
-        <app-footer></app-footer>
-      </footer>
-      `,
-  styles: [
-
-  ],
+  templateUrl: './home-layout.component.html',
+  styleUrls: ['./home-layout.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeLayoutComponent {
