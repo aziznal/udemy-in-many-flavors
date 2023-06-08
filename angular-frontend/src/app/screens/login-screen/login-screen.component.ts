aa54148eb74926f0fef1717ac882c4e17e6d9b10
Aziz <aziznal.dev@gmail.com>
2023-06-08 22:19:50 +0300

    angular: setup UI for login screen

--- angular-frontend/src/app/screens/login-screen/login-screen.component.ts
+++ angular-frontend/src/app/screens/login-screen/login-screen.component.ts
@@ -2,13 +2,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-login-screen',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './login-screen.component.html',
   styleUrls: ['./login-screen.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginScreenComponent {
-
-}
+export class LoginScreenComponent {}
