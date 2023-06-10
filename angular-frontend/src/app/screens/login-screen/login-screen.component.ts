54caa147939ecf71fac169a9173223d7ce202958
Aziz <aziznal.dev@gmail.com>
2023-06-10 10:40:08 +0300

    angular: add link to register screen in login screen

--- angular-frontend/src/app/screens/login-screen/login-screen.component.ts
+++ angular-frontend/src/app/screens/login-screen/login-screen.component.ts
@@ -1,11 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppRoutes } from 'src/routes';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './login-screen.component.html',
   styleUrls: ['./login-screen.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginScreenComponent {}
+export class LoginScreenComponent {
+  get AppRoutes() {
+    return AppRoutes;
+  }
+}
