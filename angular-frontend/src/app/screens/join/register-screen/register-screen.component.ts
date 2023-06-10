408613ee92bb52e97a86e10fdbb5d024dc65761a
Aziz <aziznal.dev@gmail.com>
2023-06-10 11:59:14 +0300

    angular: create register screen UI

--- angular-frontend/src/app/screens/join/register-screen/register-screen.component.ts
+++ angular-frontend/src/app/screens/join/register-screen/register-screen.component.ts
@@ -1,11 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AppRoutes } from 'src/routes';
+import { PasswordStrengthMeterComponent } from 'src/app/components/password-strength-meter/password-strength-meter.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, PasswordStrengthMeterComponent],
   templateUrl: './register-screen.component.html',
   styleUrls: ['./register-screen.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterScreenComponent {}
+export class RegisterScreenComponent {
+  get AppRoutes() {
+    return AppRoutes;
+  }
+}
