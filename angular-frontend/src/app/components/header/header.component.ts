d4519435079fa6ecdafda8cd5a01d6b31d9ac4b0
Aziz <aziznal.dev@gmail.com>
2023-06-10 10:18:53 +0300

    angular: add links to login and signup pages in header

--- angular-frontend/src/app/components/header/header.component.ts
+++ angular-frontend/src/app/components/header/header.component.ts
@@ -1,12 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AppRoutes } from 'src/routes';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  get AppRoutes() {
+    return AppRoutes;
+  }
+}
