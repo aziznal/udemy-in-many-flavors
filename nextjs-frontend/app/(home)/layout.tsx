c3aa9a6893177889c6a3345a09850a90c814ea97
Aziz <aziznal.dev@gmail.com>
2023-06-13 21:48:45 +0300

    nextjs: setup basic layout and components structure

---
+++ nextjs-frontend/app/(home)/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+
+      <main>{children}</main>
+
+      <Footer />
+    </>
+  );
+}
