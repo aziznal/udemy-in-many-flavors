386da010b099452a0860f5aac6efd057b49cd031
Aziz <aziznal.dev@gmail.com>
2023-06-17 21:52:17 +0300

    nextjs: improve company images section responsiveness

--- nextjs-frontend/components/Footer.tsx
+++ nextjs-frontend/components/Footer.tsx
@@ -43,7 +43,7 @@ export default function Footer() {
 
 function CompanyLogoList() {
   return (
-    <div className="flex gap-4">
+    <div className="flex flex-wrap gap-4">
       <Image
         width="100"
         height="100"
