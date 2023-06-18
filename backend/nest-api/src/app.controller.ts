ec0989b04aa9b4fa2c9065dd37cc7362d50ff52b
Aziz <aziznal.dev@gmail.com>
2023-06-18 23:19:45 +0300

    backend: add authenticated hello-world endpoint for testing

--- backend/nest-api/src/app.controller.ts
+++ backend/nest-api/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from './modules/auth/auth.guard';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller()
 export class AppController {
-  constructor(private appService: AppService) {}
+  constructor(private appService: AppService, private jwtService: JwtService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('secure-hello')
+  @UseGuards(AuthGuard)
+  getSecureHello(@Req() req: Request): string {
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    const decodedToken = this.jwtService.decode(token) as { email: string };
+
+    return `this is a secure hello to the user with email ${decodedToken.email}`;
   }
 }
