268d9b896b58312cdfc7993d06d1e5326f2e3306
Aziz <aziznal.dev@gmail.com>
2023-06-29 17:49:42 +0300

    backend: add check for token expiration in authorized user decorator

--- backend/nest-api/src/utils/decorators/authorized-user.ts
+++ backend/nest-api/src/utils/decorators/authorized-user.ts
@@ -13,14 +13,20 @@ const expectedJwtPayload = z
 
 export type UserTokenPayload = z.infer<typeof expectedJwtPayload>;
 
+export const TO_MILLISECONDS = 1000;
+
 export const AuthorizedUser = createParamDecorator<never, never, UserTokenPayload>((_data, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest<Request & { headers: { authorization?: string } }>();
 
   if (!request.headers.authorization) return;
 
   try {
-    const tokenPayload = decode(request.headers.authorization.replace('Bearer ', ''));
-    return expectedJwtPayload.parse(tokenPayload);
+    const rawToken = decode(request.headers.authorization.replace('Bearer ', ''));
+    const token = expectedJwtPayload.parse(rawToken);
+
+    if (token.exp * TO_MILLISECONDS < Date.now()) throw new UnauthorizedException('expired jwt');
+
+    return token;
   } catch (e: unknown) {
     throw new UnauthorizedException('bad jwt');
   }
